84d0e5571e54fa14f8ef4374905bcc1a294422e2
Evette Telyas <etelyas@gmail.com>
2019-09-06 10:12:35 -0600

    update spec for multiple machines per snack

--- spec/features/user_can_see_snack_show_page_spec.rb
+++ spec/features/user_can_see_snack_show_page_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe "When a user visits the snack show page", type: :feature do
   scenario "they see snack details" do
     evette = Owner.create(name: "Evette")
     mac1 = evette.machines.create(location: "Turing School")
-    sn1 = mac1.snacks.create(name: "Twizzlers", price: 12.5)
+    mac2 = evette.machines.create(location: "Not Turing School")
+    sn1 = Snack.create(name: "Twizzlers", price: 12.5, machines: [mac1, mac2])
     sn2 = mac1.snacks.create(name: "M&M", price: 1.5)
     sn3 = mac1.snacks.create(name: "Candytown", price: 3.5)
 
@@ -13,7 +14,10 @@ RSpec.describe "When a user visits the snack show page", type: :feature do
     expect(page).to have_content(sn1.name)
     expect(page).to have_content(sn1.price)
     expect(page).to have_content(mac1.location)
+    expect(page).to have_content(mac2.location)
     expect(page).to have_content(mac1.avg_snack_price.round(2))
+    expect(page).to have_content(mac2.avg_snack_price.round(2))
     expect(page).to have_content(mac1.snack_count)
+    expect(page).to have_content(mac2.snack_count)
   end
 end
